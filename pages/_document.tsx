ca608e24541541a535e908d5139ab3dcf3eb67f8
nbc7 <90021751+nbc7@users.noreply.github.com>
2022-09-23 19:04:43 -0300

    vercel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
 
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
         </Head>
         <body>
           <Main />
